b7182d0c6d0d62c4c7c6ac97c942208038fad30e
AnthonyH0216 <60409102+AnthonyH0216@users.noreply.github.com>
2020-02-06 17:26:11 -0500

    adds new sketch sheet

---
+++ self_portrait/sketch2.js
@@ -0,0 +1,14 @@
+
+
+function setup() {
+	createCanvas(640, 360);
+}
+
+	//shapes that are only 
+
+function draw() {
+	background("blue");
+	ellipse(320, 180, 100)
+	triangle(320, 190, 240, 110,270, 180)
+	triangle(320, 190, 400, 110,370, 180)
+}
