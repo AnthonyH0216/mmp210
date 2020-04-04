72cd10a6a980a92bd23bc5ab7b68e49ea3279806
AnthonyH0216 <60409102+AnthonyH0216@users.noreply.github.com>
2020-04-04 17:46:18 -0400

    adds new lines of code to meme 4 file

--- Meme_4/sketch.js
+++ Meme_4/sketch.js
@@ -1,8 +1,15 @@
 /*
-	meme version 4
+	meme version 4 demo
 	transformation + animation
 	3.18.2020
 */
+
+//global scope
+var catImage;
+//preload loads any external assets like images or sounds
+function preload(){
+	catImage = loadImage("CAT.jpg");
+}
 
 function setup () {
 	createCanvas(500,500);
@@ -11,4 +18,31 @@ function setup () {
 
 function draw() {
 	background(0);
+
+	// mmp 210 text styles
+	fill('plum');
+	textSize(80);
+	textFont('Helvetica');
+	textAlign(CENTER, CENTER); //Centers (left and right) and centers (top and bottom) on rotation
+
+	//first transformation
+	translate(width/2, height/2); //changes canvas loacation (can put pivot point on different areas)
+
+
+	imageMode(CENTER);
+	image(catImage, 0,0);
+
+	//calculates rotation angle
+	var r = frameCount * PI/100; // rotation speed (/100 determines speed)
+	console.log(r);
+
+
+	//push and pop keep functions seperated with out affecting each other.
+	push(); //keeps (shearY) on its own
+	shearY(r);
+	text("MMP 210", 0, 0); //(0,0) = determines pivot point length
+	pop(); //keeps (shearX) unaffected by shear Y
+
+	shearX(r);
+	text("MMP 210", 0, 0);
 }
