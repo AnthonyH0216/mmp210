54ee2045cf0a39fd208f666757bfb86b4ba36302
AnthonyH0216 <60409102+AnthonyH0216@users.noreply.github.com>
2020-04-04 20:09:12 -0400

    adds line of code and scaling to meme 4 file

--- Meme_4/sketch.js
+++ Meme_4/sketch.js
@@ -6,6 +6,9 @@
 
 //global scope
 var catImage;
+var s = 1;// scale for cat image 
+var speed = 0.01; // speed for scale
+
 //preload loads any external assets like images or sounds
 function preload(){
 	catImage = loadImage("CAT.jpg");
@@ -28,9 +31,19 @@ function draw() {
 	//first transformation
 	translate(width/2, height/2); //changes canvas loacation (can put pivot point on different areas)
 
+	push()
+	scale(s);
+	s += speed; // rzte of animation (scales up image)
 
 	imageMode(CENTER);
 	image(catImage, 0,0);
+
+	if (s>2 || s<1) { 
+		speed *= -1; 
+	}//0.01 * -1 = -0.01 (this shrinks the image)
+	// the || (or symbol) prevents the image from flipping
+
+	pop() 
 
 	//calculates rotation angle
 	var r = frameCount * PI/100; // rotation speed (/100 determines speed)
